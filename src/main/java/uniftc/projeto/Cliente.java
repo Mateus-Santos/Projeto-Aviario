d603772b79d7fb1102ff8f0764b5626c9a23c5cc
Juliel098 <julielsantos9317@gmail.com>
2022-05-04 22:48:02 -0300

    Update

---
+++ src/main/java/uniftc/projeto/Cliente.java
@@ -0,0 +1,37 @@
+package uniftc.projeto;
+
+
+
+public class Cliente {
+    private int id_cliente;
+    private String data_cadastro;
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(String data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+    
+    void ConsultarPedido(){
+        
+        
+    }
+    
+    void ConsultarAviario(){
+        
+        
+    }
+    
+    
+    
+}
